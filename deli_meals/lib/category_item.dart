0ba67ce9c7f98f427b9aded8e805f15ade6bcd65
Paul Smith <paulrsmithjnr@gmail.com>
2021-06-08 15:22:25 -0500

    Switched to using named routes for navigation

--- deli_meals/lib/category_item.dart
+++ deli_meals/lib/category_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './category_meals.dart';
+import 'category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
@@ -10,12 +10,12 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen(id, title);
-        },
-      ),
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: {
+        'id': id,
+        'title': title,
+      },
     );
   }
 
