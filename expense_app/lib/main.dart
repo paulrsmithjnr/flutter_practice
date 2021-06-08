6fd221e3b0bd8d4aed6a4e2c5c28f1bfdd5f135d
Paul Smith <paulrsmithjnr@gmail.com>
2021-06-08 00:11:07 -0500

    Screen is a bit more responsive. Added relative measurements

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -106,30 +106,45 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Personal Expenses',
-          // style: TextStyle(
-          //   fontFamily: 'OpenSans',
-          // ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.add,
-            ),
-            onPressed: () => _startAddNewTransaction(context),
-          )
-        ],
+    final appBar = AppBar(
+      title: Text(
+        'Personal Expenses',
+        // style: TextStyle(
+        //   fontFamily: 'OpenSans',
+        // ),
       ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.add,
+          ),
+          onPressed: () => _startAddNewTransaction(context),
+        )
+      ],
+    );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Chart(_recentTransactions),
-            TransactionList(_userTransactions, _deleteTransaction),
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.3,
+              child: Chart(_recentTransactions),
+            ),
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context)
+                          .padding
+                          .top) /*subtracts height of app bar and status bar (above app bar from overall screen height)*/ *
+                  0.7,
+              child: TransactionList(_userTransactions, _deleteTransaction),
+            ),
           ],
         ),
       ),
