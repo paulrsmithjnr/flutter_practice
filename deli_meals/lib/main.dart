a4cc3c7cd4f00820dde5edd8d5f9fd2a0fd8dd20
Paul Smith <paulrsmithjnr@gmail.com>
2021-06-08 15:41:51 -0500

    Mades some minor edits to named route

--- deli_meals/lib/main.dart
+++ deli_meals/lib/main.dart
@@ -30,9 +30,11 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold,
             )),
       ),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/', //default is '/'
       routes: {
-        '/category-meals': (ctx) => CategoryMealsScreen(),
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
       },
     );
   }
