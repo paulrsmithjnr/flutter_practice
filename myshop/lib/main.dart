90668e88cf9b47e5c7b86384ddcb6bef17fdd97d
Paul Smith <paulrsmithjnr@gmail.com>
2021-06-10 19:37:00 -0500

    Favourite button now works

--- myshop/lib/main.dart
+++ myshop/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:myshop/screens/product_detail_screen.dart';
 import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
--- myshop/lib/screens/product_detail_screen.dart
+++ myshop/lib/screens/product_detail_screen.dart
@@ -9,11 +9,13 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productId =
         ModalRoute.of(context).settings.arguments as String; //product id
+
     final loadedProduct = Provider.of<Products>(
       context,
       listen:
           false, //to prevent screen from updating everytime a product changes - essentially using Provider as a way of accessing data
     ).findById(productId);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
