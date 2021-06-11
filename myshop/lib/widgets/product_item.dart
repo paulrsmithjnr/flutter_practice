b69be0206f84b5db33b3d2e6abcaf53101c23c5c
Paul Smith <paulrsmithjnr@gmail.com>
2021-06-11 14:33:20 -0500

    Added a snackbar and a confirmation dialog

--- myshop/lib/widgets/product_item.dart
+++ myshop/lib/widgets/product_item.dart
@@ -54,6 +54,20 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context)
+                  .hideCurrentSnackBar(); //removes any existing snackbars
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Added item to cart!'),
+                  duration: Duration(seconds: 3),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
