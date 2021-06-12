da07e8a814fdfad85a3baa40e0b439f5f4703993
Paul Smith <paulrsmithjnr@gmail.com>
2021-06-12 05:30:19 -0500

    Added pull-to-refresh pattern tothe 'Your Products' screen

--- myshop/lib/screens/user_products_screen.dart
+++ myshop/lib/screens/user_products_screen.dart
@@ -9,6 +9,13 @@ import './edit_product_screen.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+    //set listen to false because we don't want to set up a listener for updates in products here
+    //(which would rerun the build method whenever there is an update in the list of products)
+    //but rather we just want to be able to trigger the fetchAndSetProducts method of the Products Provider class we created
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -25,20 +32,23 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (_, i) => Column(
-            children: [
-              UserProductItem(
-                productsData.items[i].id,
-                productsData.items[i].title,
-                productsData.items[i].imageUrl,
-              ),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (_, i) => Column(
+              children: [
+                UserProductItem(
+                  productsData.items[i].id,
+                  productsData.items[i].title,
+                  productsData.items[i].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
+            itemCount: productsData.items.length,
           ),
-          itemCount: productsData.items.length,
         ),
       ),
     );
