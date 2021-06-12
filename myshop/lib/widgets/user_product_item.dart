61539c72839b133c9239dd46a732b368eb586a97
Paul Smith <paulrsmithjnr@gmail.com>
2021-06-11 21:10:23 -0500

    Delete functionality now works

--- myshop/lib/widgets/user_product_item.dart
+++ myshop/lib/widgets/user_product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
 import '../screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -27,7 +29,9 @@ class UserProductItem extends StatelessWidget {
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
             ),
