7faafe6cdba7c761bf24ecfc6ef23080cf99d327
Paul Smith <paulrsmithjnr@gmail.com>
2021-06-13 16:28:34 -0500

    Created chatapp and connected it to firebase

---
+++ chat_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:chat_app/screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
---
+++ chat_app/lib/screens/chat_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/vI0fIyjg6f0iRfwwVzPF/messages')
+            .snapshots(),
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (context, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/vI0fIyjg6f0iRfwwVzPF/messages')
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
+      ),
+    );
+  }
+}
